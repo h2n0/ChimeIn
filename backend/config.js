53ed630d540c26abf972116728b9c47519ca4275
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2018-11-24 14:19:04 +0000

    Fixing config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   clientId: "a93b57996bdc421bb998540cb90654f5",
-  clientSecret: "09d9a6fd64274d86bc4e99edbfbdd057",
+  clientSecret: "SECRET",
   redirectUri: "http://localhost:8888/auth"
 }
