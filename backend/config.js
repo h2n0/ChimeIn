e10994c9cfe3e28b7303c3967a038a7713c24fe2
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2018-11-24 14:14:12 +0000

    ?

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  clientId: "ClientID",
-  clientSecret: "clientSecret",
-  redirectUri: "redirect"
+  clientId: "a93b57996bdc421bb998540cb90654f5",
+  clientSecret: "09d9a6fd64274d86bc4e99edbfbdd057",
+  redirectUri: "http://localhost:8888/auth"
 }
