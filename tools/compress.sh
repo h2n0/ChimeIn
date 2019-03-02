2c7fc5f73e3adc3ed58077da648d0ac410c7dbbe
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2019-03-02 16:30:31 +0000

    Nothing much

--- tools/compress.sh
+++ tools/compress.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-
+echo "Running build script"
 folder="./public/scripts"
 files=$(ls $folder | grep ".js")
-
 for f in $files; do
 	name=$(echo "$f" | awk -F. '{print $1}')
 	echo "Uglifying: $name > ${name}.min.js"
 	uglifyjs "${folder}/${f}" > "${folder}/min/${name}.min.js"
 done
-
-
 folder="./public/style"
-sass "$folder/main.scss" "./public/style.out.css"
-rm "$temp"
-#echo "Done"
+
+printf "Compiling CSS... "
+sass "$folder/main.scss" "./public/style.out.css" > /dev/null
+if [ $? -eq 0 ]; then
+	printf "Done"
+else
+	printf "Failed!"
+fi
+printf "\n"
+echo "Done"
